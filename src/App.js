049c35fb9337d574cf90fbbf9669dd63f14b268e
Steven Kristjan <s.rekand@khk.ee>
2022-10-27 13:11:56 +0300

    CRUD project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { AddComment } from "./components/AddComment";
+import { Comment } from "./components/Comment";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10")
+      .then((response) => response.json())
+      .then((data) => setComments(data))
+      .catch((error) => console.log(error));
+  };
+
+  const onAdd = async (name, email, body) => {
+    await fetch("https://jsonplaceholder.typicode.com/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        body: body
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => {
+        if (response.status !== 201) {
+          return;
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setComments((comments) => [...comments, data]);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onEdit = async (id, name, email, body) => {
+    await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        body: body
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        // setUsers((users) => [...users, data]);
+        const updatedComments = comments.map((comment) => {
+          if (comment.id === id) {
+            comment.name = name;
+            comment.email = email;
+          }
+
+          return comment;
+        });
+
+        setComments((comments) => updatedComments);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onDelete = async (id) => {
+    await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`, {
+      method: "DELETE"
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        } else {
+          setComments(
+            comments.filter((comment) => {
+              return comment.id !== id;
+            })
+          );
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Comments</h1>
+      <AddComment onAdd={onAdd} />
+      {comments.map((comment) => (
+        <Comment
+          id={comment.id}
+          key={comment.id}
+          name={comment.name}
+          email={comment.email}
+          body={comment.body}
+          onEdit={onEdit}
+          onDelete={onDelete}
+        />
+      ))}
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Comment.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export const Comment = ({ name, email, body, id, onEdit, onDelete }) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleEdit = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  const handleOnEditSubmit = (evt) => {
+    evt.preventDefault();
+    onEdit(id, evt.target.name.value, evt.target.email.value, evt.target.body.value);
+    setIsEdit(!isEdit);
+  };
+
+  return (
+    <div>
+      {isEdit ? (
+        <form onSubmit={handleOnEditSubmit}>
+          <input placeholder="Name" name="name" defaultValue={name} />
+          <input placeholder="Email" name="email" defaultValue={email} />
+          <input placeholder="Body" name="body" defaultValue={body} />
+          <button onSubmit={handleOnEditSubmit}>Save</button>
+        </form>
+      ) : (
+        <div className="user">
+          <span className="user-name">{name}</span>
+          <span className="user-email">{email}</span>
+          <span className="user-body">{body}</span>
+          <div>
+            <button onClick={handleEdit}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
